4ff443cb2c4f1447105a6160de75e98c7c238aaa
Lucas Henrique <henrique.lucas521@gmail.com>
2023-12-30 00:01:47 -0300

    create users module, users routes file, users controllers files and users services directory

---
+++ backend/src/modules/users/controllers/index.ts
@@ -0,0 +1,11 @@
+import { findAll } from './find-all';
+import { findByName } from './find-by-name';
+import { create } from './create';
+import { update } from './update';
+
+export const UserController = {
+  findAll,
+  findByName,
+  create,
+  update,
+};
---
+++ backend/src/modules/users/users.routes.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import { UserController } from './controllers';
+
+const router = express.Router();
+
+router.route('/').get(UserController.findAll);
+router.route('/:userName').get(UserController.findByName);
+router.route('/').post(UserController.create);
+router.route('/:userName').put(UserController.update);
