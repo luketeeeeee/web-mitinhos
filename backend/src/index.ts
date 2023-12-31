36d14e218b40be76795438bb5a299d683995d576
Lucas Henrique <henrique.lucas521@gmail.com>
2023-12-31 00:38:35 -0300

    turn profile picture to an optional collumn for users, export user routes object with the first functional route (create users)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import express, { Response } from 'express';
 import cors from 'cors';
 import log from './utils/logger';
 
-// import de rotas
+import { usersRoutes } from './modules/users';
 
 const port = 8080;
 const url = `http://localhost:${port}`;
@@ -12,8 +12,8 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
+app.use('/users', usersRoutes);
+
 app.listen(port, () => {
   log.info(`servidor iniciado em ${url}`);
 });
-
-export default app;
--- backend/src/modules/users/users.routes.ts
+++ backend/src/modules/users/users.routes.ts
@@ -3,7 +3,9 @@ import { UserController } from './controllers';
 
 const router = express.Router();
 
-router.route('/').get(UserController.findAll);
-router.route('/:userName').get(UserController.findByName);
+// router.route('/').get(UserController.findAll);
+// router.route('/:userName').get(UserController.findByName);
 router.route('/').post(UserController.create);
-router.route('/:userName').put(UserController.update);
+// router.route('/:userName').put(UserController.update);
+
+export default router;
