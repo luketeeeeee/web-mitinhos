0858317a96db797e2e7c060fef270fade691e029
Lucas Henrique <henrique.lucas521@gmail.com>
2024-01-27 14:50:55 -0300

    add home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
+import { Home } from './pages/Home';
 import { Month3Letter } from './pages/Month3Letter';
 
 function App() {
 	const routes = createBrowserRouter([
+		{
+			path: '/',
+			element: <Home />,
+		},
 		{
 			path: '/feliz-90-dias',
 			element: <Month3Letter />,
---
+++ frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+import { Link } from 'react-router-dom';
+
+export const Home = () => {
+	return (
+		<div>
+			<Link to="/feliz-90-dias">oi clique aqui zé</Link>
+		</div>
+	);
+};
