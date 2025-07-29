0b30c34b386c81e72916c62fa93b32b403f7df08
Lucas Henrique <henrique.lucas521@gmail.com>
2025-07-29 00:23:10 -0300

    fix

--- frontend/src/hooks/users.hook.ts
+++ frontend/src/hooks/users.hook.ts
@@ -1,23 +1,23 @@
-import { create } from 'zustand';
-import { UserType } from '../types';
+// import { create } from 'zustand';
+// import { UserType } from '../types';
 
-type useUsersProps = {
-	loggedUser: UserType;
-	login: (userData: { username: string; password: string }) => void;
-	logout: () => void;
-};
-
-export const useUsers = create<useUsersProps>((set) => ({
-	loggedUser: {
-		username: '',
-		token: '',
-	},
-	//
-	login: async (userData) => {
-		try {
-			// request
-		} catch (error) {}
-	},
-	//
-	logout: () => {},
-}));
+// type useUsersProps = {
+// 	loggedUser: UserType;
+// 	login: (userData: { username: string; password: string }) => void;
+// 	logout: () => void;
+// };
+//
+// export const useUsers = create<useUsersProps>((set) => ({
+// 	loggedUser: {
+// 		username: '',
+// 		token: '',
+// 	},
+// 	//
+// 	login: async (userData) => {
+// 		try {
+// 			// request
+// 		} catch (error) {}
+// 	},
+// 	//
+// 	logout: () => {},
+// }));
